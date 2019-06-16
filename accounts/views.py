5bf5ba7fb15ac2b319ae9b47e3fe6a268c563c1f
ah-med <ahmedomosanya@gmail.com>
2019-06-16 21:17:57 +0100

    add logout and navbar auth

--- accounts/views.py
+++ accounts/views.py
@@ -63,8 +63,10 @@ def login(request):
 
 
 def logout(request):
+  if request.method == 'POST':
+    auth.logout(request)
+    messages.success(request, 'You are now logged out')
     return redirect('index')
-
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
